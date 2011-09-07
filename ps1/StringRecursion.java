a8f4e1b742f56cb458ee4aa2d20a51333dcb0b0b
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-09-07 16:59:35 -0400

    WIP: int indexOf(char, String) implemented

--- ps1/StringRecursion.java
+++ ps1/StringRecursion.java
@@ -34,12 +34,51 @@ public class StringRecursion {
         }
     }
     
+    /**
+     * Print letters recursivelly. The comma might have to be handled in a better way.
+     * @param str
+     */
+    
+    public static int indexOf(char ch, String str){
+        
+        if(str.length() <= 0){
+            return -1;
+        }
+        
+        //check if string is empty, nothing has been found (or nothing can be found)
+        if(ch == str.charAt(0)){
+            return 0;
+        }
+        // recursion
+        else{
+            int index = 1;
+            String substring = str.substring(1, str.length());
+            // not sure if it is the best way to do it,
+            // in case we reach the end of the string without finding the character
+            if(indexOf(ch, substring) == -1){
+                return -1;
+            }
+            return index += indexOf(ch, substring);
+            }
+    }
+    
     public static void main(String[] args) {
         System.out.println("Spelling \"Rabbit\"");
         printLetters("Rabbit");
+        char i = 'i';
+        int index = indexOf(i, "Rabbit");
+        System.out.println("Index Of \"i\" in \"Rabbit\" is:" + index);
         System.out.println("Spelling \"I like to recurse!\"");
         printLetters("I like to recurse!");
+        char p = 'P';
+        index = indexOf(p, "I like to recurse!");
+        System.out.println("Index Of \"P\" in \"I like to recurse!\" is:" + index);
         System.out.println("Spelling \"\"");
         printLetters("");
+        char f = 'f';
+        index = indexOf(f, "");
+        System.out.println("Index Of \"f\" in \"\" is:" + index);
+        
+        
     }
 }
