76d8de4083246ae5dc514a53d23836b28fb8caec
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-10-04 14:52:02 -0400

    WIP: Programming Problem 2 done!

--- ps2/Median.java
+++ ps2/Median.java
@@ -33,8 +33,30 @@ public class Median {
      * whatever initial parameters make sense.
      */
     public static void findMedian(int[] arr) {
-        // add an appropriate call to your recursive
+      fMedian(arr, 0, arr.length - 1);
+      
+      // even and odd arrays
+      int median = arr[arr.length/2];
+      System.out.println("median value 1: " + median);
+      
+      // if even array
+      if(arr.length % 2 == 0){
+        int median2 = arr[(arr.length/2)-1];
+        System.out.println("median value 2: " + median2);
+        }
     }
+    
+    /* fMedian - recursive method that does the work for findMedian */
+    private static void fMedian(int[] arr, int first, int last) {
+        int split = partition(arr, first, last);
+        
+        // only process left sub-array if split > 
+        if (first < split && split >= arr.length/2 - 1)
+          fMedian(arr, first, split);// left subarray
+        if (last > split + 1 && split <= arr.length/2 - 1)
+          fMedian(arr, split + 1, last); // right subarray
+    }
+    
     
     /* 
      * Put the definition of your recursive median-finding method below. 
@@ -49,8 +71,17 @@ public class Median {
         // the median of this array is the average of 15 and 18 = 16.5
         int[] evenLength = {4, 18, 12, 34, 7, 42, 15, 22, 5, 27};
         
+        System.out.println("*****************");
+        System.out.println("odd length: ");
+        Sort.printArray(oddLength);
+        findMedian(oddLength);
+        Sort.printArray(oddLength);
         
-        /* Put code to test your method here. */
+        System.out.println("*****************");
+        System.out.println("even length: ");
+        Sort.printArray(evenLength);
+        findMedian(evenLength);
+        Sort.printArray(evenLength);
         
     }
 }
